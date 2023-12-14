051395b97da89a1f07a1fa6544e3b97f261aa8ae
Gokberklk <gokberk_nn@hotmail.com>
2023-12-14 23:46:58 +0200

    delete some redundant line

--- src/mock.js
+++ src/mock.js
@@ -3,10 +3,8 @@
 import { API, graphqlOperation } from 'aws-amplify';
 import { createEvent, createAdmin, createUser} from './graphql/mutations';
 import { listEvents, listUsers, listAdmins } from './graphql/queries';
-// Function to delete all records
 export const deleteAllRecords = async () => {
     try {
-        // Delete all events
         const eventsResult = await API.graphql(graphqlOperation(listEvents));
         const eventsToDelete = eventsResult.data.listEvents.items;
         await Promise.all(
@@ -14,8 +12,6 @@ export const deleteAllRecords = async () => {
                 await API.graphql(graphqlOperation(deleteEvent, { input: { id: event.id } }));
             })
         );
-
-        // Delete all users
         const usersResult = await API.graphql(graphqlOperation(listUsers));
         const usersToDelete = usersResult.data.listUsers.items;
         await Promise.all(
@@ -23,8 +19,6 @@ export const deleteAllRecords = async () => {
                 await API.graphql(graphqlOperation(deleteUser, { input: { id: user.id } }));
             })
         );
-
-        // Delete all admins
         const adminsResult = await API.graphql(graphqlOperation(listAdmins));
         const adminsToDelete = adminsResult.data.listAdmins.items;
         await Promise.all(
@@ -39,47 +33,51 @@ export const deleteAllRecords = async () => {
     }
 };
 
-// Function to add mock records
 export const addMockRecords = async () => {
     try {
-        // Add mock events
         const mockEvent1 = {
             input: {
-                // Add mock event data
-                timeAndDate: new Date().toISOString(),
+                timeAndDate: '2023-12-31T12:00:00Z',
                 eventName: 'Mock Event 1',
-                // ... other event fields
-            }
+                eventPoster: 'mock_event1.jpg',
+                place: 'Mock Place 1',
+                price: 10.0,
+                capacity: 100,
+                eventPlanner: 'Mock Planner 1',
+                description: 'This is a mock event description 1.',
+            },
         };
         await API.graphql(graphqlOperation(createEvent, mockEvent1));
 
         const mockEvent2 = {
             input: {
-                // Add mock event data
-                timeAndDate: new Date().toISOString(),
+                timeAndDate: '2023-12-31T15:00:00Z',
                 eventName: 'Mock Event 2',
-                // ... other event fields
-            }
+                eventPoster: 'mock_event2.jpg',
+                place: 'Mock Place 2',
+                price: 15.0,
+                capacity: 50,
+                eventPlanner: 'Mock Planner 2',
+                description: 'This is a mock event description 2.',
+            },
         };
         await API.graphql(graphqlOperation(createEvent, mockEvent2));
 
-        // Add mock admins
         const mockAdmin1 = {
             input: {
-                // Add mock admin data
-                username: 'mock_admin_1',
-                // ... other admin fields
-            }
+                username: 'mock_admin1',
+            },
         };
         await API.graphql(graphqlOperation(createAdmin, mockAdmin1));
 
-        // Add mock users
         const mockUser1 = {
             input: {
-                // Add mock user data
+                studentId: '123456',
                 name: 'Mock User 1',
-                // ... other user fields
-            }
+                surname: 'UserSurname',
+                METUmail: 'mockuser1@example.com',
+                yoS: 2,
+            },
         };
         await API.graphql(graphqlOperation(createUser, mockUser1));
 
@@ -89,11 +87,9 @@ export const addMockRecords = async () => {
     }
 };
 
-
-// GraphQL query to list events
 export const listMockEvents = async () => {
     try {
-        // Mock data for events
+
         const mockEvents = [
             {
                 eventId: '1',
@@ -126,8 +122,6 @@ export const listMockEvents = async () => {
                 __typename: 'Event',
             },
         ];
-
-        // Return the mock events
         return mockEvents;
     } catch (error) {
         console.error('Error listing mock events:', error);
